ff36fc5624f4f367eb163cb72868515089d18917
WisniewskiGabriel <gabriel_wisniewski@gmail.com>
2024-05-26 23:24:25 -0300

    tentar print de vars do deployment custom

--- testing_args.py
+++ testing_args.py
@@ -4,7 +4,8 @@ from prefect import flow, task, get_run_logger, variables
 @flow(name="Show args", log_prints=True)
 def print_args():
     print("test")
-    x = variables.get()
+    x = variables.get("MY-NEW-ENV_VAR")
+    print(x)
 
 
 if __name__ == "__main__":
