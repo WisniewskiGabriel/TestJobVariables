68734537ba036c8477be6822e706cf8780e0187e
WisniewskiGabriel <gabriel_wisniewski@gmail.com>
2024-05-26 23:11:02 -0300

    parameters add

--- testing_args.py
+++ testing_args.py
@@ -1,9 +1,10 @@
-from prefect import flow, task, get_run_logger
+from prefect import flow, task, get_run_logger, variables
 
 
 @flow(name="Show args", log_prints=True)
 def print_args():
     print("test")
+    x = variables.get()
 
 
 if __name__ == "__main__":
